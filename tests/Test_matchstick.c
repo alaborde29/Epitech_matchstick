5260b08b439db1bd1b93d9f8a739e57706b7f0ce
LABORDE Alexandre <alaborde@localhost.localdomain>
2021-05-18 12:01:09 +0200

    another one (test lol)

--- tests/Test_matchstick.c
+++ tests/Test_matchstick.c
@@ -8,10 +8,22 @@
 #include "matchstick.h"
 #include <criterion/criterion.h>
 
-Test(check_victory, not_win)
+Test(check_ac_size, ko)
 {
     int ac = 1;
     char **av = 0;
 
     cr_assert_eq(-1, error_handling(ac, av));
+}
+
+Test(check_ac_size, ok)
+{
+    int ac = 3;
+    char *av[3];
+
+    av[1] = "4";
+    av[2] = "4";
+    av[0] = 0;
+
+    cr_assert_eq(0, error_handling(ac, av));
 }
